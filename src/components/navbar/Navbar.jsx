df36436faefd485dca816ac70d75223a6ad52808
mustafa mehel <mehelx5207@gmail.com>
2022-11-08 17:04:01 +0300

    aaddd

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,14 +1,21 @@
 import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import { logOut } from "../../auth/firebase";
 import { AuthContext } from "../../context/AuthContext";
 import NavbarStyled, { NavLink, MenuDiv, Btn } from "./Navbarstyle";
 
 const Navbar = () => {
+  const navigate = useNavigate();
   const { currentUser } = useContext(AuthContext);
   console.log(currentUser);
   return (
     <NavbarStyled>
-      <div style={{ color: "white", fontSize: "25px" }}>React Movie APP</div>
+      <div
+        style={{ color: "white", fontSize: "25px", cursor: "pointer" }}
+        onClick={() => navigate("/")}
+      >
+        React Movie APP
+      </div>
 
       <MenuDiv>
         {currentUser ? (
@@ -16,7 +23,9 @@ const Navbar = () => {
             <p style={{ color: "white", fontSize: "1.5rem" }}>
               {currentUser?.email.slice(0, 5)}
             </p>
-            <NavLink onClick={() => logOut()}>Logout</NavLink>
+            <NavLink to="/" onClick={() => logOut()}>
+              Logout
+            </NavLink>
           </>
         ) : (
           <>

ea5ab9a2418fd21868ce72e19856427140e9f32b
mustafa mehel <mehelx5207@gmail.com>
2022-11-09 19:05:04 +0300

    errr

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -3,38 +3,42 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import Moviecard from "../../components/moviecard/Moviecard";
 import MainStyled, { SearchBar, InputBar, Search } from "./Mainstyled";
+import { AuthContext } from "../../context/AuthContext";
+import { toastWarnNotify } from "../../helper/ToastNotify";
 
 const Main = () => {
   const [movie, setMovie] = useState("");
   const [searchBar, setSearchBar] = useState("");
+  const { currentUser } = useContext(AuthContext);
   const API_KEY = "a1438880f784e9a353cf7ea5917e3916";
   const url = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
   const urlSearch = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${searchBar}`;
+  useEffect(() => {
+    getApi(url);
+  }, []);
 
-  const getApi = () => {
+  const getApi = (API) => {
     axios
-      .get(url)
+      .get(API)
       .then((res) => {
         // console.log(res.data.results);
         setMovie(res.data.results);
       })
       .catch((err) => console.log(err));
   };
-  useEffect(() => {
-    getApi();
-  }, []);
-  const getSearch = () => {
-    axios
-      .get(urlSearch)
-      .then((res) => {
-        console.log(res);
-        setMovie(res.data.results);
-      })
-      .catch((err) => console.log(err));
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    if (searchBar && currentUser) {
+      getApi(urlSearch);
+      console.log(searchBar);
+      setSearchBar("");
+    } else if (!currentUser) {
+      toastWarnNotify("please log in to search movie");
+    } else {
+      toastWarnNotify("please enter a task");
+    }
   };
-  useEffect(() => {
-    getApi();
-  }, []);
 
   return (
     <div>
@@ -45,7 +49,7 @@ const Main = () => {
             setSearchBar(e.target.value);
           }}
         />
-        <Search onClick={() => getSearch()}>search</Search>
+        <Search onClick={handleChange}>search</Search>
       </SearchBar>
       <MainStyled>
         {movie?.length > 0 &&
